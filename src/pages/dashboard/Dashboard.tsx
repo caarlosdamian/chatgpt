c6df2484d292a5fcd4a5508087ea6aca5011b323
caarlosdamian <dapecarlos@gmail.com>
2024-08-03 17:34:35 -0600

    feat(pages): added dashboard page styling

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,36 @@
-'./Dashboard.css'
+import './Dashboard.css';
 
 export const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="./logo.png" alt="logo"/>
+          <h1>Carlos AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="./chat.png" alt="" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="./image.png" alt="" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="./code.png" alt="" />
+            <span>Help me with Code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form action="">
+          <input type="text" placeholder="Ask me anything..." />
+          <button>
+            <img src="./arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
